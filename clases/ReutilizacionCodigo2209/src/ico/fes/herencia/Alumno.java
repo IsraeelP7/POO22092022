98ed2e57f4152aa224104d34526796822aaa42a7
IsraeelP7 <israel18223@gmail.com>
2022-03-29 13:00:17 -0600

    codigo29Marzo

--- clases/ReutilizacionCodigo2209/src/ico/fes/herencia/Alumno.java
+++ clases/ReutilizacionCodigo2209/src/ico/fes/herencia/Alumno.java
@@ -50,4 +50,18 @@ public class Alumno extends Persona {
     public void estudiar(int cantidad){
         System.out.println(this.getNombre()+ "está estudiando" + cantidad + "Hrs");
     }
+    //polimorfismo por sobre escritura
+    @Override
+    public void dormir(){
+        System.out.println("El estudiante " +
+                                        this.getNombre() + "Esta durmiendo 5 horas...");
+    }
+    
+    //polimorfismo por sobre carga
+    
+    public void dormir( int horasEstudio ){
+        int horasDormir = 8;
+        System.out.println( this.getNombre() +
+                "esta durmiendo " + (horasDormir - horasEstudio) + " Horas");
+    }
 }
---
+++ clases/ReutilizacionCodigo2209/src/reutilizacioncodigo2209/PruebaPolimorfismo.java
@@ -0,0 +1,25 @@
+
+package reutilizacioncodigo2209;
+
+import ico.fes.herencia.Alumno;
+import ico.fes.herencia.Persona;
+
+public class PruebaPolimorfismo {
+    
+    public static void main(String[] args) {
+        System.out.println("Hola Polimorfismo");
+        Persona per1 = new Persona();
+        per1.setNombre("Jose ");
+        per1.dormir();
+        
+        Alumno al1 = new Alumno();
+        al1.setNombre("Jose ");
+        al1.dormir();
+        al1.dormir( 4 );
+        
+        
+        
+        
+    }
+    
+}
