b358fa939db629e982be3df0f8aca19ca19990ae
IsraeelP7 <israel18223@gmail.com>
2022-04-26 13:03:35 -0500

    clase 26 de Abril

--- clases/JavaBasico2209/src/javabasico2209/JavaBasico2209.java
+++ clases/JavaBasico2209/src/javabasico2209/JavaBasico2209.java
@@ -5,6 +5,7 @@
 package javabasico2209;
 
 import java.awt.Color;
+import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
 /**
@@ -73,12 +74,14 @@ public class JavaBasico2209 {
         int edad5 = 19;
         System.out.println( edad5 < 18? "Menor de edad 5": "Ya! el tequila");
         
-        int val1 = 1;
-        int val2 = 2;
+        int val1 = 1;    // 4 bytes --> 32 bits
+        int val2 = 2;     //
         //Haremos la comparacion a nivel de bits
         /*
         val1 = 0000...0001;
         val2 = 0000...0010;
+                 or   0011    
+                and   0000
         1 or 0 = 1
         1 or 1= 1
         0 or 0= 0
@@ -96,7 +99,7 @@ public class JavaBasico2209 {
               0000...0011 => 3
         */
         
-        int val3 = 1; //0000...0001
+        int val3 = 1; //0000...0001          0000...0100 --> representa un 4 binario
         //int val4 <<= val3;
         //System.out.println(val3 <<= );
         int val4 = 0;
@@ -141,7 +144,9 @@ public class JavaBasico2209 {
         lista[3] = new Alumno("88888", 2, 8.0f);
         lista[4] = new Alumno("66666", 2, 6.0f);
         
+        System.out.println("+d.-.-.-..-.-.-..-");
         for (int i = 0; i < lista.length; i++) {
+            System.out.println( lista[i].evaluarDesempenio());
             Alumno alumno = lista[i];
             System.out.println( alumno.evaluarDesempenio() );
         }
@@ -156,6 +161,58 @@ public class JavaBasico2209 {
             System.out.println(alumno.evaluarDesempenio());
             
         }
+        
+        ArrayList<Alumno> grupo2209= new ArrayList<Alumno>();
+        grupo2209.add( new Alumno("99999", 2, 9.0f));
+        grupo2209.add( new Alumno("88888", 2, 8.0f));
+        grupo2209.add( new Alumno("77777", 2, 7.0f));
+        grupo2209.add( new Alumno("66666", 2, 6.0f));
+        grupo2209.add( new Alumno("55555", 2, 5.0f));
+        
+        for (Alumno alumno : grupo2209) {
+            System.out.println(alumno);
+        }
+        
+        System.out.println("Add con indice");
+        grupo2209.add(2, new Alumno("81111", 3, 9.9f));
+        
+        for (Alumno alumno : grupo2209) {
+            System.out.println(alumno);
+            
+        }
+        
+        
+        Alumno tmp =grupo2209.get(3); //NC = 77777
+        System.out.println("Alumno index = 1 :" + tmp);
+        
+        System.out.println("ELiminar el index 3");
+        Alumno tmp2 = grupo2209.remove(3);
+        System.out.println("Elemento sacado = " + tmp2);
+        
+        for (Alumno alumno : grupo2209) {
+            System.out.println(alumno);
+            
+        }
+        
+        System.out.println("Reemplazar el i-eseimo elemento");
+        Alumno tmp3 = grupo2209.set(0, new Alumno("44444", 4, 4.0f));
+        System.out.println("El sacado es= " + tmp3);
+        
+        for (Alumno alumno : grupo2209) {
+            System.out.println(alumno);
+            
+        }
+        
+        //Excepciones
+        try {
+             System.out.println("Excepciones");
+             System.out.println(grupo2209.get(20));
+            
+        } catch (Exception e) {
+            System.out.println("Error... revisa los indices");
+        }
+        
+        System.out.println("Fin del programa");
        
     }
         
